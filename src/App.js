ab3340cef5bd48418bdb7ed59d73e42b7726df1f
rgabeflores <rgabeflores@gmail.com>
2021-03-04 23:19:18 -0800

    Created basic react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+import { Layout, Button, Typography } from 'antd';
+
+const { Content } = Layout;
+const { Title } = Typography;
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <Layout className="App">
+            <Content className="links-list-container">
+                <Title className="links-list-title">Gabe Flores</Title>
+                <ul className="links-list">
+                    <li><Button type="link" href="https://rgabeflores.com" target="_blank" rel="noopener noreferrer">Website</Button></li>
+                    <li><Button type="link" href="" rel="noopener noreferrer" size="large">Link</Button></li>
+                    <li><Button type="link" href="/" target="_blank" rel="noopener noreferrer">Link</Button></li>
+                    <li><Button type="link" href="/" target="_blank" rel="noopener noreferrer">Link</Button></li>
+                    <li><Button type="link" href="https://rgabeflores.darkroom.tech/" target="_blank" rel="noopener noreferrer">Photography Prints</Button></li>
+                </ul>
+            </Content>
+        </Layout>
     );
 }
 
